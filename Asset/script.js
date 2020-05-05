8a6d26ce9361df2da9d30d5e78677d5f706e213d
LionelNg-CI <lincoln.values.adding@gmail.com>
2020-05-05 21:08:24 +0800

    setting

---
+++ Asset/script.js
@@ -0,0 +1,13 @@
+//start with document ready
+$(function () {
+  let apiURL =
+    "https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=SGD&to_symbol=USD&apikey=RV76S9OYG1OF0XEU";
+  let apiKey = "ZCD6PLAXN5830VI9";
+
+  $.ajax({
+    url: apiURL,
+    // data: {function:"FX_DAILY","from_symbol":"SGD","to_symbol":"USD",
+    // apiKey:apiKey},
+    contentType: "application/json; charset=utf-8",
+  }); //end ajax call
+});
