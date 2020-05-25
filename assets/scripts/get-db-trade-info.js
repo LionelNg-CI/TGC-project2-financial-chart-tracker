6f2b9f6c3ec36edcc8d5d187f996c81ea6126ca8
LionelNg-CI <lincoln.values.adding@gmail.com>
2020-05-26 07:50:32 +0800

    test js

--- assets/scripts/get-db-trade-info.js
+++ assets/scripts/get-db-trade-info.js
@@ -1,11 +1,11 @@
 var settings = {
   async: true,
   crossDomain: true,
-  url: "https://portfolio-63ea.restdb.io/rest/import-trade-history",
+  url: "https://portfolio-63ea.restdb.io/rest/trade-history",
   method: "GET",
   headers: {
     "content-type": "application/json",
-    "x-apikey": "5ecc486e4a532801892ed7f3",
+    "x-apikey": "<5ecc486e4a532801892ed7f3>",
     "cache-control": "no-cache",
   },
 };
