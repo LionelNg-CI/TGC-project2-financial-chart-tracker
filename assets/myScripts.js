49809f456107918ef34a7476f413bee406693559
LionelNg-CI <lincoln.values.adding@gmail.com>
2020-06-02 09:38:46 +0800

    working on summation of each coins balance

--- assets/myScripts.js
+++ assets/myScripts.js
@@ -62,21 +62,22 @@ $.ajax(settingsTrade).done(function (response) {
   $("#totaltrades").replaceWith(totalCount);
 });
 
-// // get coins balance
-// let settingsBal = {
-//   async: true,
-//   crossDomain: true,
-//   url: "https://portfolio-63ea.restdb.io/rest/trade-history",
-//   method: "GET",
-//   headers: {
-//     "content-type": "application/json",
-//     "x-apikey": "5ecc486e4a532801892ed7f3",
-//     "cache-control": "no-cache",
-//   },
-// };
-// $.ajax(settingsBal).done(function (response) {
-//   // let response.filter("fee");
-// });
+// get coins balance
+let settingsBal = {
+  async: true,
+  crossDomain: true,
+  url: "https://portfolio-63ea.restdb.io/rest/trade-history",
+  method: "GET",
+  headers: {
+    "content-type": "application/json",
+    "x-apikey": "5ecc486e4a532801892ed7f3",
+    "cache-control": "no-cache",
+  },
+};
+$.ajax(settingsBal).done(function (response) {
+  let bal = response.filter(["fee"]);
+  // console.log();
+});
 
 // canvasjs scripts
 window.onload = function () {
